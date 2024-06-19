d5050ee66fec8204dd03479f8c5c4979b56c41e7
ruchikaroy <contactruchikaroy@gmail.com>
2024-06-19 08:59:30 +0100

    This commit will complete task 7.

--- src/components/ArticleDetailPage.jsx
+++ src/components/ArticleDetailPage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { Heading, Text, Box, Image } from "@chakra-ui/react";
+import { Heading, Text, Box, Image, Button, Flex } from "@chakra-ui/react";
+import { FaThumbsUp } from "react-icons/fa";
+import { FaThumbsDown } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import CommentsPage from "./CommentsPage";
@@ -9,9 +11,43 @@ function ArticleDetailPage() {
   const [article, setArticle] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  const handleIncrementVotes = () => {
+    const originalArticle = { ...article };
+    setArticle({ ...article, votes: article.votes + 1 });
+    axios
+      .patch(
+        `https://news-api-ibvn.onrender.com/api/articles/${article.article_id}`,
+        {
+          inc_votes: 1,
+        }
+      )
+      .then(response)
+      .catch((error) => {
+        if (error) {
+          setArticle(originalArticle);
+        }
+      });
+  };
+
+  const handleDecrementVotes = () => {
+    setArticle({ ...article, votes: article.votes - 1 });
+    axios
+      .patch(
+        `https://news-api-ibvn.onrender.com/api/articles/${article.article_id}`,
+        {
+          inc_votes: -1,
+        }
+      )
+      .then(response)
+      .catch((error) => {
+        if (error) {
+          setArticle(originalArticle);
+        }
+      });
+  };
+
   useEffect(() => {
     if (id) {
-      console.log(id);
       setIsLoading(true);
 
       axios
@@ -46,6 +82,33 @@ function ArticleDetailPage() {
       </Text>
       <Image src={article.article_img_url} alt={article.title} />
       <Text mt={4}>{article.body}</Text>
+      <Flex alignItems="center" mt={4}>
+        <Text>Votes: {article.votes}</Text>
+        <Button
+          onClick={() => handleIncrementVotes(article)}
+          variant="link"
+          _hover={{
+            transform: "scale(1.05)",
+            transition: "transform .15s ease-in",
+            cursor: "thumbnail",
+            color: "#68cf8a",
+          }}
+        >
+          <FaThumbsUp />
+        </Button>{" "}
+        <Button
+          onClick={() => handleDecrementVotes(article)}
+          variant="link"
+          _hover={{
+            transform: "scale(1.05)",
+            transition: "transform .15s ease-in",
+            cursor: "thumbnail",
+            color: "#68cf8a",
+          }}
+        >
+          <FaThumbsDown />
+        </Button>{" "}
+      </Flex>
       <CommentsPage id={id} />
     </Box>
   );
